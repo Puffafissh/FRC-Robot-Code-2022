f8a68361e3bc600042c8a3f031289fdd6ef3748b
Puffafissh <tv2290@columbia.edu>
2022-04-15 16:57:47 +1000

    can ID

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -14,10 +14,10 @@ package frc.robot;
  */
 public final class Constants {
 
-    public static final int MOTOR_LEFT_1_ID = 0;
-    public static final int MOTOR_RIGHT_1_ID = 1;
+    public static final int MOTOR_LEFT_1_ID = 1;
+    public static final int MOTOR_RIGHT_1_ID = 3;
     public static final int MOTOR_LEFT_2_ID = 2;
-    public static final int MOTOR_RIGHT_2_ID = 3;
+    public static final int MOTOR_RIGHT_2_ID = 4;
     public static final int DRIVER_CONTROLLER = 0;
     public static final int XBOXCONTROLLER = 0;
     public static final int LOW_FIRE_VALUE = 0;
